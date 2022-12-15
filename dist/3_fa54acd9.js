786d0e41ede55152b05b38e0cf39285b225cb672
xinkuiwu <1079894635@qq.com>
2022-12-15 16:08:31 +0800

    feat: eslint

--- dist/3_fa54acd9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{13:function(n,e,t){"use strict";t.r(e);var i=t(0),a=t.n(i);e.default=function(){return a.a.createElement("div",null,"dynamic import")}}}]);
--- webpack.prod.js
+++ webpack.prod.js
@@ -57,7 +57,9 @@ module.exports = {
         rules:[
             {
                 test: /.js$/,
-                use: 'babel-loader'
+                use: ['babel-loader',
+                'eslint-loader'
+            ]
             },
             {
                 test: /.css$/,
