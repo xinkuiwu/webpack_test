9758881a9919b91b817710838254a2a742cb1f70
xinkuiwu <1079894635@qq.com>
2022-12-15 12:18:06 +0800

    feat: dynamic import

--- src/search/index.js
+++ src/search/index.js
@@ -8,11 +8,32 @@ import '../../common/index'
 import {a} from './tree-shaking'
 
 class Search extends React.Component {
+    constructor(){
+        super(...arguments)
+
+        this.state = {
+            Text :null
+        }
+    }
+
+    loadCompentent() {
+        import('./text.js').then((text)=> {
+            this.setState(
+                {
+                    Text: Text.default
+                }
+            )
+        })
+    }
     render() {
         const aStr = a()
+        const {Text} = this.state
         return <div className="search-text">
             {aStr}
-            <img src={logo}></img>
+            {
+                Text?<Text></Text>: null
+            }
+            <img src={logo} onClick={this.loadCompentent}></img>
              Search Text搜索s</div>
     }
 }
